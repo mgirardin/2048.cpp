8183189cdefdce27a61f0b255f20ff9adadd8dd0
mgirardin <matheus.girardin@gmail.com>
2020-09-02 10:31:19 -0300

    Create .gitignore

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-g++ -c board.cpp
-g++ -c main.cpp
-g++ -c game_session.cpp
-g++ -c cli.cpp
+g++ -c src/board.cpp
+g++ -c src/main.cpp
+g++ -c src/game_session.cpp
+g++ -c src/cli.cpp
 g++ main.o game_session.o board.o cli.o -o prod
 rm cli.o main.o game_session.o board.o 
 ./prod
